e09e54af785a7716374284ea74aebef3792b3be8
BillyJonathan29 <billyjonathan048@gmail.com>
2024-12-13 12:07:01 +0700

    updateSubmited

--- app/Http/Controllers/SubmitedTaskController.php
+++ app/Http/Controllers/SubmitedTaskController.php
@@ -7,30 +7,33 @@ use App\Models\Submited;
 use App\Models\Task;
 use App\MyClass\Response;
 use App\MyClass\Validations;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class SubmitedTaskController extends Controller
 {
-    public function index(Task $task){
-            if ($task) {
-                $submited = Submited::where('task_id', $task->id)->get();
-            } else {
-                $submited = [];
-            }
+    public function index(Task $task)
+    {
+        if ($task) {
+            $submited = Submited::where('task_id', $task->id)->get();
+        } else {
+            $submited = [];
+        }
 
-            $data = [
-                'totalPengumpulan' => $submited ? $submited->count() : 0,
-                'soalTugas' => $task ? $task->task : '-'
-            ];
-            return view('submited.index', [
-                'data' => $data,
-                'submited' => $submited
-            ]);
+        $data = [
+            'totalPengumpulan' => $submited ? $submited->count() : 0,
+            'soalTugas' => $task ? $task->task : '-'
+        ];
+        return view('submited.index', [
+            'data' => $data,
+            'submited' => $submited
+        ]);
     }
 
-    public function create(Request $request, Task $task){
+    public function create(Request $request, Task $task)
+    {
         Validations::createSubmited($request);
         DB::beginTransaction();
         try {
@@ -43,16 +46,27 @@ class SubmitedTaskController extends Controller
                 return Response::success([
                     'message' => 'Anda Sudah Mengumpulkan Tugas',
                 ]);
-            }else {
+            } else {
+                // Ambil file PDF dari request
+                $pdfFile = $request->file('file');
+
+                // Membuat nama file berdasarkan tanggal upload
+                $timestamp = Carbon::now()->format('Y-m-d');
+                $fileName = $task->task . "_" .  $timestamp . '.' . $pdfFile->getClientOriginalExtension();
+
+                $path = $pdfFile->storeAs('public/task_submited', $fileName);
+
                 $data = [
                     'user_id' => $request->user()->id,
                     'task_id' => $task->id,
-                    'file' => $request->file,
+                    'file' => $fileName,
                 ];
-                Submited::createSubmited($data);
+                $submited = Submited::createSubmited($data);
 
+                DB::commit();
                 return Response::success([
-                    'message' => 'Tugas Berhasil Di Upload'
+                    'message' => 'Tugas Berhasil Di Upload',
+                    'path' => $path
                 ]);
             }
         } catch (Exception $e) {
--- app/Models/Submited.php
+++ app/Models/Submited.php
@@ -9,42 +9,46 @@ class Submited extends Model
 {
     use HasFactory;
 
+    protected $guarded = [];
+
     /**
      * @return Relasi
      */
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function task(){
-        return $this->belongsTo(Task::class. 'task_id');
+    public function task()
+    {
+        return $this->belongsTo(Task::class . 'task_id');
     }
 
     /**
      * Create Update delete
      */
-    public static function createSubmited($request){
-        $data = self::create($request->all());
-        $data->saveFile($request->file);
-
+    public static function createSubmited($request)
+    {
+        $data = self::create($request);
         return $data;
     }
 
     // public function FilePath()
-	// {
-	// 	return storage_path('app/public/storage/submited/' . $this->file);
-	// }
+    // {
+    // 	return storage_path('app/public/storage/submited/' . $this->file);
+    // }
 
-    public function saveFile($request){
-        if ($request->hasFile('file')) {
-			$file = $request->file('file');
-			$filename = date('YmdHis_') . $file->getClientOriginalName();
-			$file->move(storage_path('app/public/storage/submited'), $filename);
-			$this->update([
-				'file' => $filename,
-			]);
-		}
+    // public function saveFile($request)
+    // {
+    //     if ($request->file('file')) {
+    //         $file = $request->file('file');
+    //         $filename = date('YmdHis_') . $file->getClientOriginalName();
+    //         $file->move(storage_path('app/public/storage/submited'), $filename);
+    //         $this->update([
+    //             'file' => $filename,
+    //         ]);
+    //     }
 
-		return $this;
-    }
+    //     return $this;
+    // }
 }
